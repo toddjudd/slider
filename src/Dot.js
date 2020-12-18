3da7d7ff5d7abd3debe158124da24a2c431f3b5b
tracer482 <Mustang69>
2020-12-17 22:40:15 -0700

    pre eject

---
+++ src/Dot.js
@@ -0,0 +1,12 @@
+import './Dot.css';
+
+const Dot = (props) => {
+  let deathThreashold = 80;
+  return (
+    <div className='Dot'>
+      <div className='icon'></div>
+    </div>
+  );
+};
+
+export default Dot;
--- src/index.js
+++ src/index.js
@@ -1,12 +1,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
+// import './index.css';
+import Dot from './Dot';
 import reportWebVitals from './reportWebVitals';
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <Dot />
   </React.StrictMode>,
   document.getElementById('root')
 );
